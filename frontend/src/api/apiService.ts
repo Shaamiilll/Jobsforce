160ed477a547864ecbb44fd24cc5921f3c18e3c3
shaamiilll <shamilamiyan@gmail.com>
2025-03-26 00:59:26 +0530

    UPDATE:Updated the HOST URL

--- frontend/src/api/apiService.ts
+++ frontend/src/api/apiService.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
+const API_URL = import.meta.env.VITE_HOST_URL || 'http://localhost:5000/api';
 const token = localStorage.getItem('token')
 
 export const uploadResume = async (file: File) => {
