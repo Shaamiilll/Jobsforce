4c2d7826a86bdbecc4dbeaa1c17ff47a1fecf9bb
shaamiilll <shamilamiyan@gmail.com>
2025-03-26 11:51:21 +0530

    Updated the vercel issue

--- ML-resume-parser/app.py
+++ ML-resume-parser/app.py
@@ -1,10 +1,9 @@
-from fastapi import FastAPI, HTTPException, UploadFile, File
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
-import os
 import requests
 import logging
+from io import BytesIO
 from extract_text import extract_text_from_pdf, extract_text_from_docx, extract_skills
-from extract_fields import extract_phone_number, extract_email_address
 
 app = FastAPI()
 logging.basicConfig(level=logging.INFO)
@@ -20,7 +19,7 @@ async def health_check():
 async def extract_skills_from_url(file_data: FileUrlRequest):
     try:
         file_url = file_data.fileUrl
-        logging.info(f"Received request body: {file_data}")  # <-- Debugging
+        logging.info(f"Received request body: {file_data}")
 
         # Download the file from the URL
         response = requests.get(file_url)
@@ -30,31 +29,19 @@ async def extract_skills_from_url(file_data: FileUrlRequest):
         content_type = response.headers.get('Content-Type')
         logging.info(f"Downloaded file content type: {content_type}")
 
-        # Extract the file extension
-        file_extension = ""
+        # Store the file in memory
+        file_bytes = BytesIO(response.content)
+
+        # Determine file type and extract text
         if content_type == "application/pdf":
-            file_extension = ".pdf"
+            text = extract_text_from_pdf(file_bytes)
         elif content_type == "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
-            file_extension = ".docx"
+            text = extract_text_from_docx(file_bytes)
         else:
             raise HTTPException(status_code=400, detail="Unsupported file type")
 
-        # Save the file temporarily
-        temp_filename = f"temp_file{file_extension}"
-        with open(temp_filename, "wb") as temp_file:
-            temp_file.write(response.content)
-            logging.info(f"File saved as {temp_filename}")
-
-        # Perform processing based on the file type
-        if file_extension == ".pdf":
-            text = extract_text_from_pdf(temp_filename)
-        else:
-            text = extract_text_from_docx(temp_filename)
-
+        # Extract skills
         skills = extract_skills(text)
-
-        # Clean up temporary files
-        os.remove(temp_filename)
 
         return {"skills": skills}
 
