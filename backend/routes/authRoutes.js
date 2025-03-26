f5a945dfba7794bd36400c64f7f2f2480c0bb925
shaamiilll <shamilamiyan@gmail.com>
2025-03-26 16:34:35 +0530

    UPDATE:Updated the backend router

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -4,7 +4,7 @@ import { registerUser, loginUser } from '../controllers/authController.js';
 
 const router = express.Router();
 
-// Routes
+// Routes of login
 router.post('/register', registerUser);
 router.post('/login', loginUser);
 
